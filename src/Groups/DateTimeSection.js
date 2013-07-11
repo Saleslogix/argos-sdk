9539c7f519a3ef4c9e0331fc19a0941f21b8e89d
chenrie <cameron.henrie@sage.com>
2013-07-11 15:26:27 -0700

    Removing Copy Right

--- src/Groups/DateTimeSection.js
+++ src/Groups/DateTimeSection.js
@@ -1,6 +1,4 @@
-/* Copyright (c) 2010, Sage Software, Inc. All rights reserved.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
+/* Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
@@ -14,7 +12,7 @@
  */
 
 /**
- * @class Sage.Platform.Mobile.Groups._GroupSection
+ * @class Sage.Platform.Mobile.Groups.DateTimeSection
  */
 define('Sage/Platform/Mobile/Groups/DateTimeSection', [
     'dojo/_base/declare',
--- src/Groups/GroupByValueSection.js
+++ src/Groups/GroupByValueSection.js
@@ -1,6 +1,4 @@
-/* Copyright (c) 2010, Sage Software, Inc. All rights reserved.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
+/* Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
@@ -14,7 +12,7 @@
  */
 
 /**
- * @class Sage.Platform.Mobile.Groups._GroupSection
+ * @class Sage.Platform.Mobile.Groups.GroupByValueSection
  */
 define('Sage/Platform/Mobile/Groups/GroupByValueSection', [
     'dojo/_base/declare',
