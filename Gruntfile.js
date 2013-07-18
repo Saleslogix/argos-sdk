f2320601acf1c398bf75748647a603276028a932
Jason Best <skeeterbug@gmail.com>
2013-07-18 16:11:23 -0700

    Added cssmin and csslint to grunt config. Added minified css for entire sdk under the min folder.

--- Gruntfile.js
+++ Gruntfile.js
@@ -22,12 +22,33 @@ module.exports = function(grunt) {
                 host: 'http://127.0.0.1:8000/',
                 template: 'GruntRunner.tmpl'
             }
+        },
+        cssmin: {
+            combine: {
+                files: {
+                    'min/css/sdk.min.css': ['content/reui/themes/sage-green/theme.css', 'content/css/base.css'],
+                    'min/css/test.min.css': ['content/reui/themes/sage-green/test.css']
+                }
+            }
+        },
+        csslint: {
+            options: {
+                csslintrc: '.csslintrc',
+                formatters: [
+                    { id: 'junit-xml', dest: 'report/junit.xml' }
+                ]
+            },
+            lax: {
+                src: ['content/**/*.css']
+            }
         }
     });
     
     grunt.loadNpmTasks('grunt-contrib-jshint');
     grunt.loadNpmTasks('grunt-contrib-connect');
     grunt.loadNpmTasks('grunt-contrib-jasmine');
+    grunt.loadNpmTasks('grunt-contrib-cssmin');
+    grunt.loadNpmTasks('grunt-contrib-csslint');
 
     grunt.registerTask('test', ['connect', 'jasmine']);
     grunt.registerTask('default', ['test']);
