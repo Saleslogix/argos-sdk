19e00b2c5d6c9b56d47dd17b1a94ee61d5fa9ccc
Jason Best <jason.best@swiftpage.com>
2013-09-12 14:53:10 -0700

    Moved the onScroll event handle to be wired up after the feed has been processed. The handle is disconnected on clear. This prevents onScroll from firing when the list is initially loading. #MBL-10192 Fixed

--- src/List.js
+++ src/List.js
@@ -726,6 +726,7 @@ define('Sage/Platform/Mobile/List', [
          * Extends dijit Widget postCreate to setup the selection model, search widget and bind
          * to the global refresh publish
          */
+        _onScrollHandle: null,
         postCreate: function() {
             this.inherited(arguments);
 
@@ -733,10 +734,6 @@ define('Sage/Platform/Mobile/List', [
                 this.set('selectionModel', new ConfigurableSelectionModel());
 
             this.subscribe('/app/refresh', this._onRefresh);
-
-            if (this.continuousScrolling) {
-                this.connect(this.domNode, 'onscroll', this.onScroll);
-            }
 
             if (this.enableSearch) {
                 var searchWidgetCtor = lang.isString(this.searchWidgetClass)
@@ -1432,6 +1429,10 @@ define('Sage/Platform/Mobile/List', [
 
             domClass.remove(this.domNode, 'list-loading');
             this.listLoading = false;
+
+            if (!this._onScrollHandle && this.continuousScrolling) {
+                this._onScrollHandle = this.connect(this.domNode, 'onscroll', this.onScroll);
+            }
         },
         /**
          * Initiates the SData request.
@@ -1599,12 +1600,16 @@ define('Sage/Platform/Mobile/List', [
                 this._selectionModel.resumeEvents();
             }
 
-            this.requestedFirstPage = false;
             this.entries = {};
             this.feed = false;
             this.query = false; // todo: rename to searchQuery
             this.defaultSearchTermSet = false;
             this.hasSearched = false;
+            
+            if (this._onScrollHandle) {
+                this.disconnect(this._onScrollHandle);
+                this._onScrollHandle = null;
+            }
 
             if (all !== false && this.searchWidget) this.searchWidget.clear();
 
