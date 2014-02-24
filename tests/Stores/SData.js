8a07b219d5061ae952dd5f3688a61e244f42af2f
Jason Best <jason.best@swiftpage.com>
2014-02-24 15:54:52 -0700

    Added SData store tests for query, add, and put.

--- tests/Stores/SData.js
+++ tests/Stores/SData.js
@@ -7,6 +7,11 @@ define('tests/Stores/SData', [
         readEntry: function(request, options) {
             options.success.call(options.scope || this, { data: '123' });
         },
+        readFeed: function(request, options) {
+            var data = { data: '123' };
+            options.success.call(options.scope || this, data);
+            return data;
+        },
         getVersion: function() {
         },
         getIncludeContent: function() {
@@ -24,6 +29,19 @@ define('tests/Stores/SData', [
         getContractName: function() {
         },
         getDataSet: function() {
+        },
+        isJsonEnabled: function() {
+            return true;
+        },
+        createEntry: function(request, data, options) {
+            var data = { data: '123' };
+            options.success.call(options.scope || this, data);
+            return data;
+        },
+        updateEntry: function(request, data, options) {
+            var data = { data: '123' };
+            options.success.call(options.scope || this, data);
+            return data;
         }
     };
 
@@ -66,6 +84,78 @@ define('tests/Stores/SData', [
             promise.then(function(results) {
                 expect(results.data).toBe('123');
                 done();
+            });
+        });
+
+        it('can query some data', function(done) {
+            var store, promise;
+            store = new Store({
+                service: mockService,
+                resourceKind: 'test',
+                resourcePredicate: 'test',
+                contractName: 'system',
+                select: ['a','b','c'],
+                include: ['1','2','3'],
+                queryName: '',
+                queryArgs: '',
+                orderBy: '',
+                idProperty: '$key',
+                applicationName: 'slx',
+                scope: this
+            });
+
+            spyOn(store.service, 'readFeed').and.callThrough();
+            promise = store.query('foo');
+            expect(store.service.readFeed).toHaveBeenCalled();
+            promise.then(function(results) {
+                done();
+            });
+        });
+
+        it('can write some data', function(done) {
+            var store, promise, allDone, count = 0;
+            store = new Store({
+                service: mockService,
+                resourceKind: 'test',
+                resourcePredicate: 'test',
+                contractName: 'system',
+                select: ['a','b','c'],
+                include: ['1','2','3'],
+                queryName: '',
+                queryArgs: '',
+                orderBy: '',
+                idProperty: '$key',
+                applicationName: 'slx',
+                scope: this
+            });
+
+            allDone = function() {
+                count = count + 1;
+                if (count === 3) {
+                    done();
+                }
+            };
+
+            // Test add
+            spyOn(store.service, 'createEntry').and.callThrough();
+            promise = store.add({});
+            expect(store.service.createEntry).toHaveBeenCalled();
+            promise.then(function(results) {
+                allDone();
+            });
+
+            // Test update
+            promise = store.put({ data: '123'}, { overwrite: true });
+            expect(store.service.createEntry).toHaveBeenCalled();
+            promise.then(function(results) {
+                allDone();
+            });
+
+            // Test with no put options
+            promise = store.put({ data: '123'});
+            expect(store.service.createEntry).toHaveBeenCalled();
+            promise.then(function(results) {
+                allDone();
             });
         });
 
@@ -135,6 +225,16 @@ define('tests/Stores/SData', [
             });
 
             expect(store.getMetadata()).toBe(null);
+        });
+
+        it('can not remove (not supported)', function() {
+            var store = new Store();
+            try {
+                store.remove();
+            } catch(err) {
+                // If we throw an error, perhaps remove was implemented and a service dependency wasn't satisfied here.
+                expect(true).toBe(false);
+            }
         });
     });
 });
