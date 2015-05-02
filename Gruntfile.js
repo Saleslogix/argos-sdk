971dfd38aec80106248aa2485d51a33d31b58035
Jason Best <jason.best@infor.com>
2015-05-01 21:58:20 -0700

    Upgrading to TypeScript 1.5, using the tsconfig.json file. Removed grunt-typescript. Due to the tsconfig.json, you can now just invoke the typescript compiler in the project directory to compile.

--- Gruntfile.js
+++ Gruntfile.js
@@ -7,7 +7,8 @@ module.exports = function(grunt) {
     grunt.loadTasks('grunt-tasks');
 
     // Register alias tasks
-    grunt.registerTask('ts', ['typescript']);
+    grunt.registerTask('ts', ['shell:typescript']);
+    grunt.registerTask('typescript', ['shell:typescript']);
     grunt.registerTask('test', ['typescript', 'connect', 'jasmine:coverage']);
     grunt.registerTask('default', ['test']);
 };
