8867122792e933a180be856b2f445cc3397440d0
Jason Best <jason.best@infor.com>
2015-07-20 21:26:47 -0700

    Re-configure tooling.

--- Gruntfile.js
+++ Gruntfile.js
@@ -7,8 +7,6 @@ module.exports = function(grunt) {
     grunt.loadTasks('grunt-tasks');
 
     // Register alias tasks
-    grunt.registerTask('ts', ['shell:typescript']);
-    grunt.registerTask('typescript', ['shell:typescript']);
-    grunt.registerTask('test', ['typescript', 'connect', 'jasmine:coverage']);
+    grunt.registerTask('test', ['connect', 'jasmine:coverage']);
     grunt.registerTask('default', ['test']);
 };
--- grunt-tasks/grunt-shell.js
+++ grunt-tasks/grunt-shell.js
@@ -1,15 +1,6 @@
 ﻿module.exports = function (grunt) {
     grunt.config('shell', {
-        typescript: {
-            command: 'node node_modules/typescript/bin/tsc',
-            options: {
-                execOptions: {
-                    cwd: '.'
-                }
-            }
-        }
     });
 
     grunt.loadNpmTasks('grunt-shell');
 };
-
