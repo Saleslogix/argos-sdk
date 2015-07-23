366e4acd359e6547736e1d0cad38f43a52dc855f
Jason Best <jason.best@infor.com>
2015-07-23 13:55:36 -0700

    Adding eslint.

---
+++ grunt-tasks/grunt-eslint.js
@@ -0,0 +1,10 @@
+module.exports = function(grunt) {
+    grunt.config('eslint', {
+        options: {
+            configFile: '.eslintrc'
+        },
+        target: ['src/**/*.js']
+    });
+
+    grunt.loadNpmTasks('grunt-eslint');
+};
