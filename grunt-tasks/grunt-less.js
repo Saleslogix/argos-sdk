1763233cf2919316a0beab8ae57418ef1c2cb5b5
Tyler Wallace <tylerw710@gmail.com>
2015-07-28 12:21:25 -0700

    updated grunt file and ran grunt less

--- grunt-tasks/grunt-less.js
+++ grunt-tasks/grunt-less.js
@@ -9,7 +9,7 @@ module.exports = function(grunt) {
         },
         production: {
             options: {
-                cleancss: true
+                compress: true
             },
             files: {
                 'min/css/themes/crm/sdk.min.crm.css': 'content/css/themes/crm.less'
@@ -19,4 +19,3 @@ module.exports = function(grunt) {
 
     grunt.loadNpmTasks('grunt-contrib-less');
 };
-
