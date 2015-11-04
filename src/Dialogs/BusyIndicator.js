dc5ede1c869acdf4c1f0857bedd88f6fd2caa056
Tyler Wallace <tylerw710@gmail.com>
2015-11-04 16:53:16 -0700

    Update busyIndicator to expose labels and modifying pathing to be correct.

--- src/Dialogs/BusyIndicator.js
+++ src/Dialogs/BusyIndicator.js
@@ -34,10 +34,12 @@ const __class = declare('argos/BusyIndicator', [ _Widget, _Templated ], {
   _busyDeferred: null,
   _busyIndicator: null,
   _progressBar: null,
+  busyIndicatorLabel: null,
   currentProgress: null,
   id: 'busyIndicator-template',
   isAsync: true,
   label: resource.loadingText,
+  progressLabel: null,
   progressText: resource.progressText,
   size: 'large',
   totalProgress: null,
@@ -53,12 +55,14 @@ const __class = declare('argos/BusyIndicator', [ _Widget, _Templated ], {
     const indicator = domConstruct.toDom(this.busyIndicatorTemplate.apply(this));
     domConstruct.place(indicator, this.busyIndicatorNode);
     this._busyIndicator = this.busyIndicatorNode.children[0];
+    this.busyIndicatorLabel = query('.busyIndicator__label', this.busyIndicatorNode)[0];
     domClass.add(this._busyIndicator, 'busyIndicator--active');
 
     if (!this.isAsync || (options.isAsync !== undefined && !options.isAsync)) {
       const progressBar = domConstruct.toDom(this.progressBarTemplate.apply({value: options.label || this.progressText}, this));
       domConstruct.place(progressBar, this.busyIndicatorNode);
       this._progressBar = query('.busyIndicator__progress__bar', progressBar)[0];
+      this.progressLabel = query('.busyIndicator__progress__label', progressBar)[0];
       this.currentProgress = options.current || 0;
       this.totalProgress = options.total || options.count || 0;
     }
