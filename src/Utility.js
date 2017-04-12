b88a273225902db9fc76a6461229181f743d2480
Jason Best <jason.best@infor.com>
2017-04-12 16:29:03 -0400

    Updating docs for encode.

--- src/Utility.js
+++ src/Utility.js
@@ -45,7 +45,6 @@ const __class = lang.setObject('argos.Utility', {
    * @return {Object} Object ready to be JSON.stringified.
    */
   sanitizeForJson: util.sanitizeForJson,
-  escape: util.escape,
 });
 
 lang.setObject('Sage.Platform.Mobile.Utility', __class);
