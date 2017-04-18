2439178e73f6d09ffe96cae2243d01158c67fb3c
pbal <pukhraj.bal@infor.com>
2017-04-18 10:42:08 -0700

    Add deps to release.cmd for jenkins

--- build/release.cmd
+++ build/release.cmd
@@ -15,6 +15,19 @@ mkdir deploy\content\dojo\dojox
 call grunt clean:css clean:js less
 call yarn run build
 
+xcopy node_modules\babel-polyfill\dist\polyfill.min.js deps\babel-polyfill\ /E /Y
+xcopy node_modules\moment\min\moment-with-locales.js deps\moment\ /E /Y
+xcopy node_modules\rx-lite\rx.lite.js deps\rx-lite\ /E /Y
+xcopy node_modules\@infor\icrm-js-common\dist\bundles\common.bundle.js deps\icrm-js-common\ /E /Y
+xcopy node_modules\@infor\icrm-js-customization\dist\bundles\customization.bundle.js deps\icrm-js-customization\ /E /Y
+xcopy node_modules\@infor\icrm-js-services\dist\bundles\services.bundle.js deps\icrm-js-services\ /E /Y
+xcopy node_modules\redux\dist\redux.min.js deps\redux\ /E /Y
+xcopy node_modules\d3\build\d3.js deps\d3\ /E /Y
+xcopy node_modules\jquery\dist\jquery.js deps\jquery\ /E /Y
+xcopy node_modules\@infor\sohoxi\dist\js\sohoxi.js deps\sohoxijs\ /E /Y
+xcopy node_modules\@infor\sohoxi\dist\css\*.css deps\sohoxicss\ /E /Y
+xcopy node_modules\@infor\sohoxi\dist\js\cultures\*.js deps\sohoxicultures\ /E /Y
+
 REM .NET Build Tool
 tools\JsBit\jsbit.exe -p "build/release.jsb2" -d "."
 
