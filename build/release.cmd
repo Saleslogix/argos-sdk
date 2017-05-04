775c8edf84cf8a472286ea58b85b156065f1466b
Jason Best <jason.best@infor.com>
2017-05-04 11:09:14 -0400

    INFORCRM-16214: Moving babel-polyfill, less, moment, redux, and rxjs to libraries from node_modules.

--- build/release.cmd
+++ build/release.cmd
@@ -15,11 +15,6 @@ mkdir deploy\content\dojo\dojox
 call grunt clean:css clean:js less
 call yarn run build
 
-xcopy node_modules\babel-polyfill\dist\polyfill.min.js deps\babel-polyfill\ /E /Y
-xcopy node_modules\moment\min\moment-with-locales.js deps\moment\ /E /Y
-xcopy node_modules\rx-lite\rx.lite.js deps\rx-lite\ /E /Y
-xcopy node_modules\redux\dist\redux.min.js deps\redux\ /E /Y
-
 REM .NET Build Tool
 tools\JsBit\jsbit.exe -p "build/release.jsb2" -d "."
 
