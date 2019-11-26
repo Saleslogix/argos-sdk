0d9a29eb400bce30497194ef47e09554d592ed3c
Jason Best <skeeterbug@gmail.com>
2019-11-26 10:19:01 -0500

    Updating to dojo 1.15

--- libraries/dojo/dojo/promise/Promise.js
+++ libraries/dojo/dojo/promise/Promise.js
@@ -5,5 +5,35 @@
 */
 
 //>>built
-define("dojo/promise/Promise",["../_base/lang"],function(b){function a(){throw new TypeError("abstract");}return b.extend(function(){},{then:function(c,b,d){a()},cancel:function(c,b){a()},isResolved:function(){a()},isRejected:function(){a()},isFulfilled:function(){a()},isCanceled:function(){a()},always:function(a){return this.then(a,a)},otherwise:function(a){return this.then(null,a)},trace:function(){return this},traceRejected:function(){return this},toString:function(){return"[object Promise]"}})});
-//# sourceMappingURL=Promise.js.map
+define("dojo/promise/Promise",["../_base/lang"],function(_1){
+"use strict";
+function _2(){
+throw new TypeError("abstract");
+};
+return _1.extend(function Promise(){
+},{then:function(_3,_4,_5){
+_2();
+},cancel:function(_6,_7){
+_2();
+},isResolved:function(){
+_2();
+},isRejected:function(){
+_2();
+},isFulfilled:function(){
+_2();
+},isCanceled:function(){
+_2();
+},always:function(_8){
+return this.then(_8,_8);
+},"catch":function(_9){
+return this.then(null,_9);
+},otherwise:function(_a){
+return this.then(null,_a);
+},trace:function(){
+return this;
+},traceRejected:function(){
+return this;
+},toString:function(){
+return "[object Promise]";
+}});
+});
