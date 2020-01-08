6483172e5a29f3e4e63fef917df6511fcd6b569b
Jason Best <skeeterbug@gmail.com>
2020-01-08 11:11:43 -0500

    Remove a dev dependency with security issues.
    A dev dependency grunt-template-jasmine-istanbul uses a vulnerable
    version of handlerbars. This is not shipped as part of our code,
    but Github's dependabot keeps bugging us about it. Also updating
    dev dependency eslint to latest version. Fix npm task for test.
    Update to latest argos-deps to address a js-yaml security issue
    child dependency.

--- Gruntfile.js
+++ Gruntfile.js
@@ -16,6 +16,5 @@ module.exports = function gruntFile(grunt) {
 
   // Register alias tasks
   grunt.registerTask('build', ['clean', 'less']);
-  grunt.registerTask('test', ['connect', 'jasmine:coverage']);
-  grunt.registerTask('test:basic', ['connect', 'jasmine:basic']);
+  grunt.registerTask('test', ['connect', 'jasmine:basic']);
 };
