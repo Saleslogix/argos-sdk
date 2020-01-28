ddcff11270feeaaa1b0bcbaf1791d41c91fe02b6
Jason Best <skeeterbug@gmail.com>
2020-01-28 09:49:43 -0500

    Add test case for decimal field changes

--- tests/Fields/DecimalFieldTests.js
+++ tests/Fields/DecimalFieldTests.js
@@ -1,102 +1,103 @@
+/* eslint-disable */
 define('tests/Fields/DecimalFieldTests', ['dojo/query','dojo/dom-attr','dojo/dom-class','argos/Fields/DecimalField'], function(query, domAttr, domClass, DecimalField) {
 return describe('Sage.Platform.Mobile.Fields.DecimalField', function() {
 
-    it('Can rouund to 2 decimal places', function() {
+    it('Can round to 2 decimal places', function() {
         var field = new DecimalField();
         var value = 0.001
         field.setValue(value);
         expect(field.getValue()).toEqual(0.00);
     });
-   
-    it('Can rouund to 2 decimal places down', function() {
+
+    it('Can round to 2 decimal places down', function() {
         var field = new DecimalField();
         var value = 0.554
         field.setValue(value);
         expect(field.getValue()).toEqual(0.55);
     });
-    it('Can rouund to 2 decimal places up', function() {
+    it('Can round to 2 decimal places up', function() {
         var field = new DecimalField();
         var value = 0.555;
         field.setValue(value);
         expect(field.getValue()).toEqual(0.56);
     });
-    it('Can rouund to 2 decimal places down', function() {
+    it('Can round to 2 decimal places down', function() {
         var field = new DecimalField();
         var value = 0.554;
         field.setValue(value);
         expect(field.getValue()).toEqual(0.55);
     });
-    it('Can rouund to 0 decimal places up', function() {
+    it('Can round to 0 decimal places up', function() {
         var field = new DecimalField();
         var value = 0.55;
         field.precision = 0;
         field.setValue(value);
         expect(field.getValue()).toEqual(1);
     });
-    it('Can rouund to 0 decimal places up', function() {
+    it('Can round to 0 decimal places up', function() {
         var field = new DecimalField();
         var value = 1000.55;
         field.precision = 0;
         field.setValue(value);
         expect(field.getValue()).toEqual(1001.00);
     });
-    it('Can rouund to 0 decimal places down', function() {
+    it('Can round to 0 decimal places down', function() {
         var field = new DecimalField();
         var value = 1000.25;
         field.precision = 0;
         field.setValue(value);
         expect(field.getValue()).toEqual(1000.00);
     });
-    it('Can rouund to 1 decimal places up', function() {
+    it('Can round to 1 decimal places up', function() {
         var field = new DecimalField();
         var value = 0.55;
         field.precision = 1;
         field.setValue(value);
         expect(field.getValue()).toEqual(0.6);
     });
-    it('Can rouund to 1 decimal places up', function() {
+    it('Can round to 1 decimal places up', function() {
         var field = new DecimalField();
         var value = 1000.55;
         field.precision = 1;
         field.setValue(value);
         expect(field.getValue()).toEqual(1000.6);
     });
-    it('Can rouund to 1 decimal places down', function() {
+    it('Can round to 1 decimal places down', function() {
         var field = new DecimalField();
         var value = 1000.24;
         field.precision = 1;
         field.setValue(value);
         expect(field.getValue()).toEqual(1000.2);
     });
-    it('Can rouund to 3 decimal places up', function() {
+    it('Can round to 3 decimal places up', function() {
         var field = new DecimalField();
         var value = 0.5568;
         field.precision = 3;
         field.setValue(value);
         expect(field.getValue()).toEqual(0.557);
     });
-    it('Can rouund to 3 decimal places up', function() {
+    it('Can round to 3 decimal places up', function() {
         var field = new DecimalField();
         var value = 1000.5559;
         field.precision = 3;
         field.setValue(value);
         expect(field.getValue()).toEqual(1000.556);
     });
-    it('Can rouund to 3 decimal places down', function() {
+    it('Can round to 3 decimal places down', function() {
         var field = new DecimalField();
         var value = 1000.24;
         field.precision = 3;
         field.setValue(value);
         expect(field.getValue()).toEqual(1000.240);
     });
-    it('Can rouund to 3 decimal places down', function() {
+    it('Can round to 3 decimal places down', function() {
         var field = new DecimalField();
         var value = 1000.2434;
         field.precision = 3;
         field.setValue(value);
         expect(field.getValue()).toEqual(1000.243);
     });
-    it('Can rouund to invaild  decimal places defaults to culture', function() {
+    it('Can round to invaild  decimal places defaults to culture', function() {
         var field = new DecimalField();
         var value = 1000.2434;
         field.precision = null;
@@ -116,5 +117,19 @@ return describe('Sage.Platform.Mobile.Fields.DecimalField', function() {
         field.setValue(value);
         expect(field.getValue()).toEqual(0);
     });
+    it('Can parse a number with currency symbol', function() {
+        var field = new DecimalField();
+        var value = '$1,000,000.00';
+        field.inputNode.value = value;
+        expect(field.getValue()).toEqual(1000000);
+
+        value = '$9,000.50';
+        field.inputNode.value = value;
+        expect(field.getValue()).toEqual(9000.50);
+
+        value = '$0.50';
+        field.inputNode.value = value;
+        expect(field.getValue()).toEqual(0.50);
+    })
 });
 });
